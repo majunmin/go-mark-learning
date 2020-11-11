92a36684fafa52038e2267bedf39731e1934e55e
majm <majm@ushareit.com>
2020-11-11 23:58:03 +0800

    [+][ go 源码文件📃]

---
+++ learn/demo/demo1.go
@@ -0,0 +1,14 @@
+package main
+
+//var name string
+//
+//func init() {
+//    flag.StringVar(&name, "name", "everyone", "The greeting object.")
+//}
+//
+///**
+// go run demo1.go
+// */
+//func learn() {
+//    fmt.Printf("Hello world, %s \n", name)
+//}
---
+++ learn/demo/demo2.go
@@ -0,0 +1,14 @@
+package main
+
+//var name = flag.String("name", "everyone", "The greeting object.")
+//
+//func init() {
+//}
+//
+///**
+//go run demo2.go -name "Robort"
+//*/
+//func main() {
+//   flag.Parse()
+//	fmt.Printf("Hello world, %s \n", *name)
+//}
